db1ae22a01de938144559c75234af7470e8e5ac4
fahim786fahim <fahim786fahim@yahoo.com>
2026-08-16 11:41:47 +0530

    Adding data details to be printed

--- model/model_training.py
+++ model/model_training.py
@@ -7,9 +7,27 @@ from tracer.paths import PROJECT_DIR
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 DATA_PATH = PROJECT_ROOT / "data" / "bank-full.csv"
 
-df = pd.read_csv(DATA_PATH)
+df = pd.read_csv(DATA_PATH, sep=";")
 
 print("Print dataset shape : ", df.shape)
 print("\nFirst five rows")
 print(df.head())
 
+#Get datatypes, column anmes, duplicates, missing values etc
+print("\n Column names : ")
+print(df.columns.tolist())
+
+print("\n Column types : ")
+print(df.dtypes)
+
+print("\n  Missing values per column : ")
+print(df.isnull().sum())
+
+print("\n Duplicate rows : ")
+print(df.duplicated().sum())
+
+print("\n Target distribution : ")
+print(df["y"].value_counts())
+
+print("\n Target distribution (%): ")
+print(df["y"].value_counts(normalize=True)*100)
